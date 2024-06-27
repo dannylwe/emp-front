563315e9df33b7511590271d0966afd9fb520ddb
Daniel Lwetabe <dannylwe11@gmail.com>
2024-06-27 15:03:58 +0300

    can login

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,23 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import CreateEmployee from './components/CreateEmployee'
+import Details from './components/Details'
+import Login from './components/Login'
+import Home from './components/Home'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <h1 className='text-3xl font-bold underline'>
-      <div>App</div>
-    </h1>
-  )
-}
+      <Router>
+          <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/login' element={<Login />} />
+              <Route path='/employee/:id' element={<Details />} />
+              <Route path='/employee/new' element={<CreateEmployee />} />
+          </Routes>
+      </Router>
+  );
+};
 
 export default App
